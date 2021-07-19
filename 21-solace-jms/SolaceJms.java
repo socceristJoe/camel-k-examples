f2795fa21a751cee9529368f52a75e0e81ad0e99
Qiao, Joe <qiao.y@pg.com>
2021-07-19 18:38:51 +0800

    solace jms

--- 21-solace-jms/SolaceJms.java
+++ 21-solace-jms/SolaceJms.java
@@ -6,45 +6,32 @@ import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.jms.JmsComponent;
 
 import javax.jms.Connection;
-import javax.jms.ConnectionFactory;
 import javax.jms.Queue;
 import javax.jms.Session;
 
-public class SolaceJms  extends RouteBuilder {
+public class SolaceJms extends RouteBuilder {
 
-        public void createQueue() throws Exception{
-            SolConnectionFactory testFact = SolJmsUtility.createConnectionFactory();
-            testFact.setHost("tcp://malta.corp.sensis.com:31253");
-            testFact.setVPN("inder");
-            testFact.setUsername("default");
-            testFact.setPassword("");
-            testFact.setDynamicDurables(true);
-            Connection connection = testFact.createConnection();
-            System.out.println("connection created");
-            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-            System.out.println("session created");
-            Queue queue = session.createQueue("createQueueTest");
-            System.out.println("Queue created");
-            session.close();
-            System.out.println("session closed");
-            connection.close();
-            System.out.println("connection closed");
-        }
+    @Override
+    public void configure() throws Exception {
 
-        @Override
-        public void configure() throws Exception {
+        SolConnectionFactory connectionFactory = SolJmsUtility.createConnectionFactory();
+        connectionFactory.setHost("tcps://mr-e3zjti4y6ai.messaging.solace.cloud:55443");
+        connectionFactory.setUsername("solace-cloud-client");
+        connectionFactory.setPassword("dnuqffh8dh8dgt4flludfrot26");
+        connectionFactory.setVPN("joepoc");
 
-            createQueue();
-            ConnectionFactory testFact =
-                    SolJmsUtility.createConnectionFactory("tcp://malta.corp.sensis.com:31253",
-                            "default","","inder",null);
+        Connection connection = connectionFactory.createConnection();
+        System.out.println("connection created");
 
-            CamelContext context = getContext();
-            context.addComponent("test", JmsComponent.jmsComponentAutoAcknowledge(testFact));
-            from("timer:ticker")
-                    .setBody()
-                    .constant("Hello from Camel-K")
-                    .to(ExchangePattern.InOnly, "test:queue:createQueueTest")
-                    .log("Messsage sent to target 1 :: ${body}");
-        }
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        System.out.println("session created");
+
+        Queue queue = session.createQueue("joequeue");
+        System.out.println("Queue created");
+
+        CamelContext context = getContext();
+        context.addComponent("solace", JmsComponent.jmsComponentAutoAcknowledge(connectionFactory));
+        from("timer:ticker").setBody().constant("Hello from Camel-K")
+                .to(ExchangePattern.InOnly, "solace:queue:joequeue").log("Messsage sent to target 1 :: ${body}");
     }
+}
