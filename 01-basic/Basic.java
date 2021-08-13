9e314f2d4a31874e234b71642801191862efebdb
Qiao, Joe <qiao.y@pg.com>
2021-08-13 20:51:26 +0800

    update for demo

--- 01-basic/Basic.java
+++ 01-basic/Basic.java
@@ -23,11 +23,11 @@ public class Basic extends RouteBuilder {
   @Override
   public void configure() throws Exception {
 
-      from("timer:java?period=1000&repeatCount=2")
+      from("timer:java?period=60000")
         .setHeader("example")
           .constant("Java")
         .setBody()
-          .simple("Hello World!!! Camel K route written in ${header.example}.")
+          .simple("Hello DevOps!!! Camel K route written in ${header.example}.")
         .to("log:info");
       
   }
--- 23-solace-amqp/SolaceAmqpProducer.java
+++ 23-solace-amqp/SolaceAmqpProducer.java
@@ -14,13 +14,13 @@ public class SolaceAmqpProducer extends RouteBuilder {
 
         ConnectionFactory connectionFactory = new JmsConnectionFactory("solace-cloud-client",
                 "dnuqffh8dh8dgt4flludfrot26", "amqps://mr-e3zjti4y6ai.messaging.solace.cloud:5671");
-        // Connection connection = connectionFactory.createConnection();
-        // Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-
+        
         CamelContext context = getContext();
         context.addComponent("solace-amqp", JmsComponent.jmsComponentAutoAcknowledge(connectionFactory));
 
-        from("timer:ticker?period=2000&repeatCount=10").setBody().constant("Hello from Camel-K")
+        from("timer:ticker?period=2000&repeatCount=10").setBody().constant("Hello from Camel-K!!")
                 .to(ExchangePattern.InOnly, "solace-amqp:topic:joepoc").log("Message sent to target 1 :: ${body}");
+                // .to(ExchangePattern.InOnly, "solace-amqp:queue:joequeue").log("Message sent to target 1 :: ${body}");
+
     }
 }
